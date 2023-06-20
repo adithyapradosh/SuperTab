5814e8411af17c0e8d73f28836b0121ec86e6154
adithya-pradosh <pradosh.adithya@gmail.com>
2023-06-21 01:14:29 +0530

    fix: grab shortcut icon

--- js/content.js
+++ js/content.js
@@ -1,33 +1,24 @@
 chrome.runtime.onMessage.addListener(function (request, sender, response) {
-	if (request.topic == 'icon') {	//get website icon
+	//grab website icon
+	if (request.topic == 'icon') {
 		var icon
 		var links = $('link[rel*="icon"]')
-		var flag = 0
 		$.each(links, function (i, val) {
-			if (val.rel == 'apple-touch-icon') {
+			if (val.sizes.value.indexOf('144') != -1) {
 				icon = val.href
-				flag = 1
-			} else if (val.sizes.value.indexOf('48') != -1) {
-				icon = val.href
-				flag = 1
+				return false
 			} else if (val.sizes.value.indexOf('96') != -1) {
 				icon = val.href
-				flag = 1
-			} else if (val.sizes.value.indexOf('144') != -1) {
-				icon = val.href
-				flag = 1
-			} else {
-				icon = val.href
-				flag = 1
-			}
-			if (flag == 1)
 				return false
+			} else if (val.sizes.value.indexOf('48') != -1) {
+				icon = val.href
+				return false
+			}
 		})
-		if (flag == 0) {
-			console.log('broken')
-			icon = window.location.href + 'favicon.ico'
-		}
 		if (icon) {
+			response({ icon: icon })
+		} else {
+			icon = window.location.href + 'favicon.ico'
 			response({ icon: icon })
 		}
 	}
